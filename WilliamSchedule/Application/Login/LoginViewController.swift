4fea2b91dd27c1bbefa21346f1b55e820af7a813
Andres Montoya <pipemontoya2.am@gmail.com>
2018-08-14 00:45:43 -0500

    finish auth with facebook + firebase

--- WilliamSchedule/Application/Login/LoginViewController.swift
+++ WilliamSchedule/Application/Login/LoginViewController.swift
@@ -9,14 +9,41 @@
 import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let loginButton = FBSDKLoginButton()
+        loginButton.delegate = self
         loginButton.center = view.center
         view.addSubview(loginButton)
     }
 
+}
+
+extension LoginViewController: FBSDKLoginButtonDelegate {
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+        Auth.auth().signInAndRetrieveData(with: credential) { (user, error) in
+            guard let user = user else {return}
+            print(user.user.displayName)
+        }
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+        
+    }
 }
