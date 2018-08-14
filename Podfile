0d4f538e497f487d845ef0281dd9f6f282d87556
Andres Montoya <pipemontoya2.am@gmail.com>
2018-08-13 21:25:56 -0500

    add firebase

--- Podfile
+++ Podfile
@@ -2,6 +2,5 @@ platform :ios, '9.0'
 
 target 'WilliamSchedule' do
   use_frameworks!
-
-
+  pod 'Firebase/Core'
 end
---
+++ Pods/Target Support Files/Pods-WilliamSchedule/Pods-WilliamSchedule-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WilliamSchedule : NSObject
+@end
+@implementation PodsDummy_Pods_WilliamSchedule
+@end
